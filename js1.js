22ccf5bcce3d6643f49368f6fa8d5cc9ab96c43c
LeDuyKhanh1989 <designer.expert228@gmail.com>
2023-05-10 09:24:58 +0700

    Update bài tập nhúng JS

--- js1.js
+++ js1.js
@@ -1,3 +1,6 @@
 alert('Hello World!')
-
+var changetext = "Xin chào JS, Tôi đã vừa thay đổi font chữ trong thẻ H2"
+function thay_chu(){
+    document.getElementById('changetext').innerHTML=changetext;
+}
 
